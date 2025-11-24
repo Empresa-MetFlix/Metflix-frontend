e2ffa94ccd6e96e23fc15a7b2ddaeef80043bcdf
= <=>
2025-11-23 23:39:20 -0300

    feat(pwa): implementando  PWA completo no projeto Metflix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import { createPinia } from "pinia"
 import App from "./App.vue"
 import router from "./router"
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {
+    console.log("Nova versão disponível");
+  },
+  onOfflineReady() {
+    console.log("App pronto para uso offline");
+  }
+})
+
 const app = createApp(App)
 
 const pinia = createPinia()
---
+++ src/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => {
+  console.log("Service worker instalado ✔️");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service worker ativo ✔️");
+});
